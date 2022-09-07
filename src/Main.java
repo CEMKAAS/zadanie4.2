633399bff11582b3bb46049ad35acf34d49f2503
= <zaroslikob@mail.ru>
2022-09-07 23:21:34 +0300

    Добавлен поиск и listOutput

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+
+
 public class Main {
+    static ArrayList<String> products = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -9,51 +13,65 @@ public class Main {
         System.out.println("1.Операция добавить");
         System.out.println("2.Операция показать");
         System.out.println("3.Операция удалить");
+        System.out.println("4.Операция поиск");
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> producs = new ArrayList<>();
-
 
         while (true) {
             System.out.println();
             System.out.println("Выберите операцию");
             String input = scanner.nextLine();
-            int nomer = Integer.parseInt(input);
+            int number = Integer.parseInt(input);
 
-            if (nomer == 1) {
+            if (number == 1) {
                 System.out.println("Какую покупку хотите сделать?");
-                String inputProducs = scanner.nextLine();
-                producs.add(inputProducs);
-                System.out.println("Итого в списке покупок: " + producs.size());
+                String inputProducts = scanner.nextLine();
+                products.add(inputProducts);
+                System.out.println("Итого в списке покупок: " + products.size());
             }
-            if (nomer == 2) {
-                System.out.println("Список покупок:");
-                for (int i = 0; i < producs.size(); i++) {
-                    String produc = producs.get(i);
-                    System.out.println((i + 1) + "." + produc);
-                }
+            if (number == 2) {
+               listOutput();
             }
 
-            if (nomer == 3) {
-                System.out.println("Список покупок:");
-                for (int i = 0; i < producs.size(); i++) {
-                    String produc = producs.get(i);
-                    System.out.println((i + 1) + "." + produc);
-                }
+            if (number == 3) {
+                listOutput();
                 System.out.println("Какую хотите удалить? Введите номер или название");
-                String input1 = scanner.nextLine();
-                //String[] parts = input1.split(" ");
-                System.out.println("Покупка " + producs.get(Integer.parseInt(input1)) + " удалена, список покупок:");
-                producs.remove(input1);
-                System.out.println("Список покупок:");
-                for (int i = 0; i < producs.size(); i++) {
-                    String produc = producs.get(i);
-                    System.out.println((i + 1) + "." + produc);
+                String delProduct = scanner.nextLine();
+                if (products.remove(delProduct)) {
+                    System.out.println("Покупка " + delProduct + " удалена, список покупок:");
+                    products.remove(delProduct);
+                    listOutput();
+                    continue;
                 }
-
+                if (!products.remove(delProduct)) {
+                    int product = Integer.parseInt(delProduct);
+                    System.out.println("Покупка " + products.get(product - 1) + " удалена, список покупок:");
+                    products.remove(product - 1);
+                    listOutput();
+                }
             }
-            continue;
 
+            if (number == 4) {
+                System.out.println("Введите текст для поиска");
+                String queryLower = scanner.nextLine();
+                queryLower = toLowerCase(queryLower);
+                System.out.println("Найдено: ");
+                for (int i = 0; i < products.size(); i++) {
+                    String itemLower = products.get(i);
+                    itemLower = toLowerCase(itemLower);
+                    if (itemLower.contains(queryLower)) {
+                        String product = products.get(i);
+                        System.out.println((i + 1) + "." + product);
+                    }
+
+                }
+            }
         }
-
+    }
+    public static void listOutput (){
+        System.out.println("Список покупок:");
+        for (int i = 0; i < products.size(); i++) {
+            String product = products.get(i);
+            System.out.println((i + 1) + "." + product);
+        }
     }
 }
